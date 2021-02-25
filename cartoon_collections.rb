33bfa1f73505cb54129db0170e7a16ceac122c29
john <john.d.leonetti@gmail.com>
2021-02-25 15:54:08 -0800

    commit tree 902eeba4c774e27d5ffcb266cffb35f1b00d288a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(dwarves)# code an argument here 
+  
+  dwarves.collect.with_index(1) do |name, num|
+    puts "#{num} #{name}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+ 
+   elements.map do |earth_elements|
+    earth_elements.capitalize + "!"
+   
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(elements)
+   
+   elements.map do |words|
+    if words.length > 4 
+    return true
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+   end #if end block
+  end # do end block
+  false
+end # method end
+
+def find_the_cheese(elements)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  elements.map do |fancy_cheese|
+       if cheese_types.include?(fancy_cheese)
+        return fancy_cheese
+       end 
+  
+    end
+    nil
 end
